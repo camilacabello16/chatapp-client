c46870e4c98fb3086cff4cadb44faa03e784e9f0
hieubd <buiduchieuuet16@gmail.com>
2022-08-10 14:55:12 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { GoogleLogin } from 'react-google-login';
+import { Alert, notification } from 'antd';
+import 'antd/dist/antd.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const responseGoogle = (e) => {
+        console.log(e);
+        console.log(1);
+        notification['success']({
+            message: 'Notification Title',
+            description:
+                'Good.',
+        });
+    }
+
+    const responseErrorGoogle = (e) => {
+        console.log(e);
+        console.log(2);
+        notification['error']({
+            message: 'Notification Title',
+            description:
+                'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+        });
+    }
+
+    return (
+        <div className="App">
+            <GoogleLogin
+                clientId="976835476824-2dm3ttto5g5d71hafahfvk84t3bjan9o.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseErrorGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
+        </div>
+    );
 }
 
 export default App;
