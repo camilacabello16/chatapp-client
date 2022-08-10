e1df64a51c13bf5c33b442e39c4c4a1dcb5bf6d8
hieubd <buiduchieuuet16@gmail.com>
2022-08-10 16:43:36 +0700

    update code
    Signed-off-by: hieubd <buiduchieuuet16@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import { GoogleLogin } from 'react-google-login';
 import { Alert, notification } from 'antd';
-import 'antd/dist/antd.css';
-
+import FacebookLogin from 'react-facebook-login';
 
 function App() {
     const responseGoogle = (e) => {
         console.log(e);
-        console.log(1);
+
         notification['success']({
             message: 'Notification Title',
             description:
@@ -17,12 +16,22 @@ function App() {
 
     const responseErrorGoogle = (e) => {
         console.log(e);
-        console.log(2);
+
         notification['error']({
             message: 'Notification Title',
             description:
                 'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
         });
+    }
+
+    const componentClicked = (e) => {
+        console.log(e);
+        console.log(1);
+    }
+
+    const responseFacebook = (e) => {
+        console.log(e);
+        console.log(2);
     }
 
     return (
@@ -33,6 +42,13 @@ function App() {
                 onSuccess={responseGoogle}
                 onFailure={responseErrorGoogle}
                 cookiePolicy={'single_host_origin'}
+            />
+            <FacebookLogin
+                appId="785433826147691"
+                autoLoad={true}
+                fields="name,email,picture"
+                onClick={componentClicked}
+                callback={responseFacebook}
             />
         </div>
     );
